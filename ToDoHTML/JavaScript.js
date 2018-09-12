3210565db896208feca6119c34f11068935bfe53
Amy Thompson <amythompson9@gmail.com>
2018-09-12 11:48:09 -0400

    Hover Over

---
+++ ToDoHTML/JavaScript.js
@@ -0,0 +1,17 @@
+﻿var lis = document.querySelectorAll("li");
+
+for (var i = 0; i < lis.length; i++){
+    lis[i].addEventListener("mouseover", function () {
+        this.style.color = "green";
+    });
+    lis[i].addEventListener("mouseout", function () {
+        this.style.color = "black";
+    });
+}
+//firstLI.addEventListener("mouseover", function() {
+//    firstLI.style.color = "green";
+//});
+
+//firstLI.addEventListener("mouseout", function () {
+//    firstLI.style.color = "black";
+//});
